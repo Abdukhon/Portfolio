12f40a9acb45203cfde021f5387f393052991fa1
yodgorov alex <yodgorovalex@gmail.com>
2026-01-02 21:53:14 +0100

    das ist mein Protfolio und da gibt's die Informationen über meine Fähigkeiten in Programmensprache und mein Lebenslauf, und also die Projekten.

---
+++ js/main-script.js
@@ -0,0 +1,8 @@
+function menuButton() {
+    var x = document.getElementById("hidden-menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
